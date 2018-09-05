8eccabd39f77cbc2c20acb58422cbb079ff1cbc2
Noel Ryan <noelgerard.ryan@gmail.com>
2018-09-05 13:33:35 +0200

    saving multiple contacts test fail

--- src/app.js
+++ src/app.js
@@ -58,7 +58,9 @@ document.addEventListener('DOMContentLoaded', () => {
             twitter: twitter.value,
         }
         console.log(`Saving the following contact: ${JSON.stringify(contact)}`);
-        storage.setItem('contacts', JSON.stringify([contact]))
+        let contacts = JSON.parse(storage.getItem('contacts')) || []
+        contacts.push(contact)
+        storage.setItem('contacts', JSON.stringify(contacts))
         renderContacts()
     })
 })
